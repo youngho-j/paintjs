7d529ce31e5a754484e9b10fdd4051c5264ac2b2
youngho-j <alfkwl1239@naver.com>
2022-06-27 00:13:52 +0900

    그림 그리기 기능 수정
    1. app.js 수정
     - 기존 코드 사용시 stroke가 되지 않는 현상 발생
     - html에서 css에 설정된 width, height 값을 가져오지 못해 안됨
     - canvas 변수에 설정된 offset 값을 대입
     - 마우스 뗐을때 선이 멈추지 않는 현상 해결(마우스 이벤트 오타로 인해
       발생)
     - 로직이 돌아가는 과정 다시 생각 해볼 것

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
+canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
@@ -6,24 +13,26 @@ function stopPainting() {
     painting = false;
 }
 
-function onMouseMove(event) {
-    const x = event.offsetX;
-    const y = event.offsetY;
-
-    console.log(x, y);
-}
-
-function onMouseDown(event) {
+function startPainting() {
     painting = true;
 }
 
-function onMouseUp(event) {
-    stopPainting();
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    
+    if(!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke();
+    }
 }
 
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseUp", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
